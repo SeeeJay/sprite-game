bc254843778cb7768ab7fff32a9b7e1aec8e1b59
SeeeJay <113055585+SeeeJay@users.noreply.github.com>
2022-09-22 14:03:52 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,18 +19,17 @@ input.onGesture(Gesture.LogoDown, function () {
 let player: game.LedSprite = null
 basic.showIcon(IconNames.Heart)
 basic.clearScreen()
+let score = 0
 basic.pause(500)
 player = game.createSprite(2, 2)
 let enemy = game.createSprite(0, 2)
 basic.forever(function () {
-    game.setScore(1)
     if (player.isTouching(enemy)) {
-        game.addScore(1)
         enemy.delete()
-        if (enemy.isDeleted()) {
-            game.addScore(game.score())
-            basic.showNumber(game.score())
-            enemy = game.createSprite(randint(0, 3), randint(0, 3))
-        }
+        game.addScore(0)
+        basic.pause(100)
+        score += 1
+        basic.showNumber(score)
+        enemy = game.createSprite(randint(3, 0), randint(0, 4))
     }
 })
